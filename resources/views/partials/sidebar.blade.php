b34602b15bb000f7043802f68d0807be6a03fdbf
yobipratama <yobir231@gmail.com>
2024-07-06 22:44:30 +0700

    menambahakn menu user dan membatasi menu berdasarkan role

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,30 +10,43 @@
                     <span class="nav-text">Dashboard</span>
                 </a>
             </li>
-            <li>
-                <a class="has-arrows" href="{{ route('admin.alternatif.index') }}">
-                    <i class="icon icon-app-store"></i>
-                    <span class="nav-text">Lokasi</span>
-                </a>
-            </li>
-            <li>
-                <a class="has-arrows" href="{{ route('admin.kriteria.index') }}">
-                    <i class="icon icon-app-store"></i>
-                    <span class="nav-text">Kriteria</span>
-                </a>
-            </li> 
-            <li>
-                <a class="has-arrows" href="{{ route('admin.penilaian.index') }}">
-                    <i class="icon icon-app-store"></i>
-                    <span class="nav-text">Penilaian</span>
-                </a>
-            </li>
-            <li>
-                <a class="has-arrows" href="{{ route('admin.penilaian.history') }}">
-                    <i class="icon icon-app-store"></i>
-                    <span class="nav-text">Riwayat Penilaian</span>
-                </a>
-            </li> 
+            @if (auth()->check() && auth()->user()->role->name == 'admin')
+                <li>
+                    <a class="has-arrows" href="{{ route('admin.owner.index') }}">
+                        <i class="icon icon-app-store"></i>
+                        <span class="nav-text">User</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="has-arrows" href="{{ route('admin.kriteria.index') }}">
+                        <i class="icon icon-app-store"></i>
+                        <span class="nav-text">Kriteria</span>
+                    </a>
+                </li>
+            @endif
+
+            @if (auth()->check() && auth()->user()->role->name == 'user')
+                <li>
+                    <a class="has-arrows" href="{{ route('user.alternatif.index') }}">
+                        <i class="icon icon-app-store"></i>
+                        <span class="nav-text">Lokasi</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="has-arrows" href="{{ route('user.penilaian.index') }}">
+                        <i class="icon icon-app-store"></i>
+                        <span class="nav-text">Penilaian</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="has-arrows" href="{{ route('user.penilaian.history') }}">
+                        <i class="icon icon-app-store"></i>
+                        <span class="nav-text">Riwayat Penilaian</span>
+                    </a>
+                </li>
+            @endif
+
+
         </ul>
     </div>
 </div>
